e92b8ac0c1b14363066076aa968930de89783cf3
Ian Chege <ianchege001@gmail.com>
2024-07-11 09:03:51 +0300

    feat: switch statement format 2

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// switch statement to evalute student's grades
+// switch statement format 1 to evalute student's grades
 
 #include <stdio.h>
 
@@ -25,6 +25,39 @@ int main()
         printf("C\n");
         break;
     case (4):
+        printf("D\n");
+        break;
+    default:
+        printf("Fail\n");
+    }
+
+    return 0;
+}
+
+// switch statement format 2 to evalute student's grades
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d, e, marks;
+    printf("Enter your grades: ");
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+    marks = (a + b + c + d + e) / 5;
+    printf("Average marks: %d\n", marks);
+
+    switch (marks)
+    {
+    case 70 ... 100:
+        printf("A\n");
+        break;
+    case 60 ... 69:
+        printf("B\n");
+        break;
+    case 50 ... 59:
+        printf("C\n");
+        break;
+    case 40 ... 49:
         printf("D\n");
         break;
     default:
